f637677c7a828cbcb273f6af7a585be15864379a
Foucauld Bellanger <63885990+Foukki@users.noreply.github.com>
2025-02-19 10:55:19 +0100

    Simple tab

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 var canvas = document.querySelector("canvas");
 var ctx = canvas.getContext("2d");
-ctx.fillStyle = "green";
-ctx.fillRect(10, 10, 100, 100);
+var tab = [[1, 0, 0],
+           [0, 1, 0],
+           [0, 0, 1]];
+
+for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
+        if (tab[i][j] == 1) {
+            ctx.fillStyle = "green";
+            ctx.fillRect(j * 10,i * 10, 10,10);
+        }
+    }
+}
