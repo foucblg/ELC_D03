60321f2e5e0a7bea7db68707d2c91ee98dd41d3d
Foucauld Bellanger <63885990+Foukki@users.noreply.github.com>
2025-02-26 08:53:44 +0100

    fix : disable selecting multiple pixels

--- front/script.js
+++ front/script.js
@@ -1,9 +1,11 @@
 var size_px = 20;
 var dim = 20;
-var id_clicked = 0
+var id_clicked = -1;
 var container = document.getElementById("gridContainer");
 container.style.gridTemplateColumns = `repeat(${dim}, ${size_px+4}px)`;
 var color_picked = "green";
+var drawed = false;
+var previous_id_clicked = -1;
 
 for (var i = 0; i < dim; i++) {
     for (var j = 0; j < dim; j++) {
@@ -22,8 +24,13 @@ for (var i = 0; i < dim; i++) {
 }
 
 function selectCanvas(i, j) {
+    if (!drawed && previous_id_clicked != -1) {
+        container.children[previous_id_clicked].style.border = "None";
+    }
     id_clicked = i*dim + j;
     container.children[id_clicked].style.border = "2px solid red";
+    previous_id_clicked = id_clicked;
+    drawed = false;
 }
 
 
@@ -34,11 +41,14 @@ function drawInCanvas(id) {
     ctx.fillStyle = color_picked;
     ctx.fillRect(0, 0, size_px+5, size_px+5);
     canvas.style.border = `2px solid ${color_picked}`;
+    id_clicked = -1;
+    drawed = true;
 }
 
 var colorBoxes = document.querySelectorAll(".color-box");
 colorBoxes.forEach(function(box) {
     box.addEventListener("click", function() {
+        if (id_clicked == -1) return;
         color_picked = box.getAttribute("data-color");
         drawInCanvas(id_clicked);
     });
