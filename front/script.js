a0315c5602b84eb057aa7b9473370c1c2a823a18
Foucauld Bellanger <63885990+Foukki@users.noreply.github.com>
2025-02-26 08:44:05 +0100

    fix : modular size

--- front/script.js
+++ front/script.js
@@ -1,7 +1,8 @@
 var size_px = 20;
-var dim = 5;
+var dim = 20;
 var id_clicked = 0
 var container = document.getElementById("gridContainer");
+container.style.gridTemplateColumns = `repeat(${dim}, ${size_px+4}px)`;
 var color_picked = "green";
 
 for (var i = 0; i < dim; i++) {
@@ -32,8 +33,7 @@ function drawInCanvas(id) {
 
     ctx.fillStyle = color_picked;
     ctx.fillRect(0, 0, size_px+5, size_px+5);
-    canvas.style.border = "2px solid " +color_picked;
-    console.log("2px solid " +color_picked)
+    canvas.style.border = `2px solid ${color_picked}`;
 }
 
 var colorBoxes = document.querySelectorAll(".color-box");
